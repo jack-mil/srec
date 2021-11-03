c297462522d89dc2c181058dd33a9be4b080d3da
jack-mil <62065280+jack-mil@users.noreply.github.com>
2021-11-03 12:36:17 -0500

    Add colors and formatting

--- srec.c
+++ srec.c
@@ -12,12 +12,12 @@ void printArray(BYTE *__buf, size_t size);
 
 int main(int argc, char *argv[]) {
 	if ( argc < 2 ) {
-		fprintf(stderr, "Usage: ./srec FILE\n");
+		fprintf(stderr, "\033[1;31mUsage: ./srec FILE\033[0m\n");
 		return 1;
 	}
 	FILE *fp = fopen(argv[1], "r");
 	if ( fp == NULL ) {
-		fprintf(stderr, "Cannot open input file \"%s\"", argv[1]);
+		fprintf(stderr, "\033[1;31mCannot open input file \"%s\"\033[0m\n", argv[1]);
 		return 1;
 	}
 
@@ -41,8 +41,9 @@ bool parse_file(FILE *fp) {
 	// `line` string will be newline-terminated
 	while ( fgets(line, sizeof(line), fp) != NULL ) {
 		lnNum++;
-		printf("-- Line %02d --\n", lnNum);
+		printf("\033[1;34m-- Line %02d --\033[0m\n", lnNum);
 		if ( !parse_line(lnNum, line) ) {
+			fprintf(stderr, "\033[1;31mError in parsing line\033[0m\n");
 			return false;
 		}
 	}
@@ -65,13 +66,13 @@ bool parse_line(unsigned lineNum, const char *line) {
 
 	// First check for leading 'S'
 	if ( line[0] != 'S' ) {
-		fprintf(stderr, "Line %d: Not a valid S-Record (first char not 'S')\n", lineNum);
+		fprintf(stderr, "\033[1;31mLine %d: Not a valid S-Record (first char not 'S')\033[0m\n", lineNum);
 		return false;
 	}
 
 	// Check record type is valid
 	if ( !isdigit(line[1]) ) {
-		fprintf(stderr, "Line %d: Unsupported record type, found : '%c'\n", lineNum, line[1]);
+		fprintf(stderr, "\033[1;31mLine %d: Unsupported record type, found : '%c'\033[0m\n", lineNum, line[1]);
 		return false;
 	}
 
@@ -102,11 +103,6 @@ bool parse_line(unsigned lineNum, const char *line) {
 		return false;
 	}
 
-	if ( checksumRead != checksumCalc ) {
-		fprintf(stderr, "Mismatched checksum 0x%02X, calculated 0x%02X\n", checksumRead, checksumCalc);
-		return false;
-	}
-
 	// Parse the record type to determine the correct address byte length
 	char rcType = line[1];
 	unsigned addr = 0;
@@ -129,7 +125,7 @@ bool parse_line(unsigned lineNum, const char *line) {
 			addrLen = '9' - rcType + 2;
 			break;
 		default:
-			printf("Unsupported S record type detected %c\n", line[1]);
+			printf("\033[1;31mUnsupported S record type detected '%c'\033[0m\n", line[1]);
 			return false;
 			break;
 	}
@@ -149,12 +145,17 @@ bool parse_line(unsigned lineNum, const char *line) {
 	}
 
 	// Print formated output
-	printf("Type %c Record, %d byte addresses\n", rcType, addrLen);
-	printf("Address: 0x%0*X\n", addrLen*2, addr); // Pad address to length is clear
-	printf("Record checksum 0x%02X: ✔\n", checksumRead);
-	printf("Data: ");
+	printf("Type: %c; %d byte address\n", rcType, addrLen);
+	printf("Address: \033[1;35m0x%0*X\033[0m\n", addrLen * 2, addr); // Pad address to length is clear
+	printf("Record checksum \033[4;3m0x%02X\033[0m: ", checksumRead);
+	if ( checksumRead != checksumCalc ) {
+		printf("\033[1;31m❌ Fail (calculated 0x%02X)\033[0m\n", checksumCalc);
+	} else {
+		printf("\033[1;32m✔ Good\033[m\n");
+	}
+	printf("Data:\033[1;33m ");
 	printArray(x, dataLen);
-	puts("");
+	puts("\033[0m");
 	return true;
 }
 
@@ -210,7 +211,7 @@ bool get_nibble(const char **s, BYTE *nb) {
 		*nb = ch - 'a' + 10;
 		return true;
 	}
-	fprintf(stderr, "Unexpected character found, '%c'", ch);
+	fprintf(stderr, "\033[1;31mUnexpected character found, '%c'\033[0m\n", ch);
 	return false;
 }
 
@@ -222,6 +223,9 @@ bool get_nibble(const char **s, BYTE *nb) {
  */
 void printArray(BYTE *buf, size_t size) {
 
+	if(size ==0 ){
+		printf("--");
+	}
 	for ( size_t i = 0; i < size; i++ ) {
 		printf("0x%02X ", *buf++);
 	}
